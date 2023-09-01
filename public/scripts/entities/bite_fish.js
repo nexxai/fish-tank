a219f427ec964280afc19e45c431cdd189bd5315
JT Smith <nexxai@gmail.com>
2023-09-01 14:59:32 -0600

    BiteFish eats fish when they get too close

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -3,4 +3,37 @@ class BiteFish extends Fish {
     super(options);
     this.imageUri = "/images/bitefish.gif";
   }
+
+  updateOneTick() {
+    let denizens = Object.keys(window.fishtank.denizens);
+
+    denizens.forEach((key) => {
+      let denizen = window.fishtank.denizens[key];
+
+      // We don't want to kill other BiteFish
+      if (
+        denizen.constructor.name.endsWith("Fish") &&
+        denizen.constructor.name !== "BiteFish"
+      ) {
+        // Is the denizen within 50 positions (pixels?) from us
+        // Just using the absolute value so we don't have to check for + AND - separately
+        if (
+          Math.abs(this.position.x - denizen.position.x) <= 50 ||
+          Math.abs(this.position.y - denizen.position.y) <= 50
+        ) {
+          // Do some logging
+          console.log("Killing | ", denizen);
+          console.log(
+            `BiteFish | x: ${this.position.x}, y: ${this.position.y}`
+          );
+          console.log(
+            `Denizen | x: ${denizen.position.x}, y: ${denizen.position.y}`
+          );
+
+          // Time to die
+          denizen.kill();
+        }
+      }
+    });
+  }
 }
